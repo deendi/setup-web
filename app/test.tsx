5482335efeabcb438d7dd61dd88b7176e757a565
deendi1 <deendi73@gmail.com>
2024-07-16 11:31:11 +0700

    menambah file baru

---
+++ app/test.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Style } from "util";
+
+export default function.Home() {
+  return {
+    <div>
+    <Head>
+    <title>Akasa WEB</title>
+
+    </Head>
+    <h1> Halo Halo Bandung
+    </h1>
+    </div>
+  }  
+}
